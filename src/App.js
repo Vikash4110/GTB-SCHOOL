6ff6945a34f91cd1bcadd8d3324ad8f5728542eb
Vikash4110 <139474262+Vikash4110@users.noreply.github.com>
2025-06-30 20:19:33 +0530

    main

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Loader from "./Components/Loader";
 import Mandatory from "./Components/Mandatory";
 import Nav from "./Components/Navbar";
 import ScrollToTop from "./Components/ScrollToTop";
-import DocumentViewer from './pages/DocumentViewer';
+import DocumentViewer from "./pages/DocumentViewer";
 import Home from "./pages/Home";
+import NotFound from './Components/ErrorPage'
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -38,6 +39,7 @@ const App = () => {
           <Route path="/mandatory" element={<Mandatory />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/:docId" element={<DocumentViewer />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </>
